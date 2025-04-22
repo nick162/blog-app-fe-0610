a06d89326d2ee8bb633c19a67b3a1a0aecaa83b6
nick162 <azwar.hanik@gmail.com>
2025-04-22 14:23:26 +0700

    feat:keeplogin

--- src/hooks/api/auth/useLogin.ts
+++ src/hooks/api/auth/useLogin.ts
@@ -1,19 +1,24 @@
 "use client";
 import { axiosInstance } from "@/lib/axios";
+import { useAuthStore } from "@/store/auth";
 import { User } from "@/types/user";
 import { useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 const useLogin = () => {
+  const router = useRouter();
+  const { onAuthSuccess } = useAuthStore();
   return useMutation({
     mutationFn: async (payload: Pick<User, "email" | "password">) => {
       const { data } = await axiosInstance.post("/auth/login", payload);
       return data;
     },
-    onSuccess: () => {
+    onSuccess: (data) => {
       toast.success("Login success");
-      // router.push("/login"); // ganti ini kalau mau redirect ke halaman lain
+      onAuthSuccess({ user: data, accessToken: data.accessToken });
+      router.push("/");
     },
     onError: (error: AxiosError<any>) => {
       toast.error(error.response?.data.massage);
