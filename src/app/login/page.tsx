b0a1a93d34eb71637a3dfad4ab4f6a2c604816e2
nick162 <azwar.hanik@gmail.com>
2025-04-22 11:04:25 +0700

    feat: add login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,6 @@
+import LoginPage from "@/features/login/page";
+const Login = () => {
+  return <LoginPage />;
+};
+
+export default Login;
--- src/hooks/api/auth/useRegister.ts
+++ src/hooks/api/auth/useRegister.ts
@@ -4,9 +4,11 @@ import { axiosInstance } from "@/lib/axios";
 import { User } from "@/types/user";
 import { useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
-import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 const useRegister = () => {
+  const router = useRouter();
   return useMutation({
     mutationFn: async (payload: Omit<User, "id">) => {
       const { data } = await axiosInstance.post("/auth/register", payload);
@@ -14,6 +16,7 @@ const useRegister = () => {
     },
     onSuccess: () => {
       toast.success("Register Success");
+      router.push("/login");
     },
     onError: (error: AxiosError<any>) => {
       toast.error(error.response?.data.message);
